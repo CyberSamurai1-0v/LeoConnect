8282185c6e4336077a369b5719f45e794a6858d4
DESKTOP-L8A9RC2\Fazna <gfsheriffdeen@gmail.com>
2025-11-27 13:50:33 +0530

    Added Navigation

---
+++ lib/screens/Member/documents.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Documents extends StatelessWidget {
+  const Documents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Documents')),
+      body: const Center(child: Text('Documents Screen')),
+    );
+  }
+}
---
+++ lib/screens/leaderboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Leaderboard extends StatelessWidget {
+  const Leaderboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Leaderboard')),
+      body: const Center(child: Text('Leaderboard Screen')),
+    );
+  }
+}
